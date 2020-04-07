fd52d2c1eaaadc745cf52433de6349031f2bc122
Aaron Janse <aaron@ajanse.me>
2020-04-06 22:00:28 -0700

    make shell.nix more idiomatic

--- shell.nix
+++ shell.nix
@@ -2,10 +2,7 @@ with import <nixpkgs>{};
 stdenv.mkDerivation rec {
     name = "multiplexer";
     buildInputs =  [ autoreconfHook pkgconfig cmake ncurses go libtsm ];
-    shellHook = ''
-        GODEBUG=cgocheck=0 go run *.go
-        exit
-    '';
 
     GOPATH="/home/ajanse/.go";
+    GODEBUG="cgocheck=0";
 }
