3fa51d43eaa90ca56d1e793d56075e55d40d719c
PotatoParser <45542237+PotatoParser@users.noreply.github.com>
2021-03-06 23:25:09 -0800

    Fix tabulation in help (#109)
    * Fix tabulation in help
    
    * Convert tabs to spaces

--- help.go
+++ help.go
@@ -10,18 +10,18 @@ const helpText = `3mux
 The terminal multiplexer inspired by i3
 
 USAGE:
-    3mux				Interactive 3mux interface
-    3mux ls             List session names (has alias '3mux ps')
-    3mux attach <name>  Attach to a session
-    3mux detach			Detach from the current session
-    3mux new <name>     Create a new session
-    3mux kill <name>    Kill a session
+    3mux                  Interactive 3mux interface
+    3mux ls               List session names (has alias '3mux ps')
+    3mux attach <name>    Attach to a session
+    3mux detach           Detach from the current session
+    3mux new <name>       Create a new session
+    3mux kill <name>      Kill a session
 
 SHORTCUTS:
-	Alt+N/Alt+Enter  Create new pane
-	Alt+Shift+Q		 Close pane
-	Alt+Shift+F		 Make pane fullscreen
-	Alt+Shift+Arrow	 Move pane
-	Alt+Arrow	     Move selection
-	Alt+/	         Toggle search
+	Alt+N/Alt+Enter   Create new pane
+	Alt+Shift+Q       Close pane
+	Alt+Shift+F       Make pane fullscreen
+	Alt+Shift+Arrow   Move pane
+	Alt+Arrow         Move selection
+	Alt+/             Toggle search
 `
