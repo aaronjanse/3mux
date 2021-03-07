6e0386b0889c3d5fc9a2eca6282b9fb205e4e004
Aaron Janse <aaron@ajanse.me>
2021-03-07 01:34:20 -0800

    remove shell.nix

--- shell.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs>{};
-stdenv.mkDerivation rec {
-    name = "multiplexer";
-    buildInputs =  [ autoreconfHook pkgconfig cmake ncurses go libtsm ];
-
-    GOPATH="/home/ajanse/.go";
-    GODEBUG="cgocheck=0";
-}
