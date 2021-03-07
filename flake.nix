aba7d10467ab6446e609ff8b971d1239eb8b2524
Aaron Janse <aaron@ajanse.me>
2021-03-07 01:25:04 -0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSystem = func: nixpkgs.lib.genAttrs systems func;
+    in
+    {
+      defaultApp = forEachSystem (system: {
+        type = "app";
+        program = "${self.defaultPackage."${system}"}/bin/3mux";
+      });
+      defaultPackage = forEachSystem (system: nixpkgs.legacyPackages."${system}".callPackage
+        ({ lib, buildGoModule, fetchFromGitHub }:
+          let inherit (import
+            (fetchFromGitHub {
+              owner = "hercules-ci";
+              repo = "gitignore";
+              rev = "c4662e662462e7bf3c2a968483478a665d00e717";
+              sha256 = "sha256-nbZfz02QoVe1yYK7EtCV7wMi4VdHzZEoPg20ZSDo9to=";
+            })
+            { inherit lib; }) gitignoreSource;
+          in
+          buildGoModule rec {
+            name = "3mux-latest";
+            execName = "3mux";
+            src = gitignoreSource ./.;
+            vendorSha256 = "sha256-tbziQZIA1+b+ZtvA/865c8YQxn+r8HQy6Pqaac2kwcU=";
+            excludedPackages = [ "fuzz" ];
+            meta = with lib; {
+              description = "Terminal multiplexer inspired by i3";
+              longDescription = ''
+                3mux is a terminal multiplexer with out-of-the-box support for
+                search, mouse-controlled scrollback, and i3-like keybindings.
+              '';
+              homepage = "https://github.com/aaronjanse/3mux";
+              license = licenses.mit;
+              # Br1ght0ne co-maintains the Nix derivation in Nixpkgs
+              maintainers = with maintainers; [ aaronjanse ];
+              platforms = platforms.unix;
+            };
+          }
+        ) { }
+      );
+    };
+}
